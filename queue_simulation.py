2279f41d4ed6fe911fc5d6cf74b5af15d7895f60
LongLeCE <38470095+LongLeCE@users.noreply.github.com>
2018-12-27 02:48:47 +0700

    Re-updated with Python "magic"

--- queue_simulation.py
+++ queue_simulation.py
@@ -3,7 +3,7 @@ from queue import Queue
 
 
 class QueueSimulation:
-    def __init__(self, process_rate, min_req_rate, max_req_rate, queue_capacity):
+    def __init__(self, process_rate, min_req_rate, max_req_rate, queue_capacity=-1):
         self.process_rate = process_rate
         self.min_req_rate = min_req_rate
         self.max_req_rate = max_req_rate
@@ -13,26 +13,19 @@ class QueueSimulation:
         results = []
         loss_req = 0
         loss_1 = self.queue.size() - self.process_rate
+        try:
+            results += self.queue.remove(self.process_rate)
+        except TypeError:
+            pass
         if loss_1 < 0:
             loss_2 = len(reqs) + loss_1
+            results += reqs[: -loss_1] if loss_2 > 0 else reqs
+            self.queue.insert(reqs[-loss_1:], is_list=True)
             if loss_2 > 0:
-                for _ in range(self.queue.size()):
-                    results += self.queue.remove()
-                results += reqs[0: -loss_1]
-                self.queue.clear()
-                for j in range(loss_2):
-                    self.queue.insert(reqs[j - loss_1])
                 loss_req += loss_2 - self.queue.size()
-            else:
-                for _ in range(self.queue.size()):
-                    results += self.queue.remove()
-                results += reqs
         else:
-            for _ in range(self.process_rate):
-                results.append(self.queue.remove())
             temp_size = self.queue.size()
-            for k in range(len(reqs)):
-                self.queue.insert(reqs[k])
+            self.queue.insert(reqs, is_list=True)
             loss_req += len(reqs) - self.queue.size() + temp_size
         return results, loss_req
 
@@ -40,7 +33,6 @@ class QueueSimulation:
         self.queue.clear()
         loss_req = 0
         for _ in range(iterations):
-            reqs = list(" " * np.random.randint(self.min_req_rate, self.max_req_rate + 1))
-            loss_req += self.step(reqs)[1]
+            loss_req += self.step(list(" " * np.random.randint(self.min_req_rate, self.max_req_rate + 1)))[1]
         return (loss_req + self.queue.size()) / iterations
     
